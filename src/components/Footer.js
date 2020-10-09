f0b49b61b4b4e52e4dc6934efb4c02e14484430d
Zergy <xxdarkbluedevilxx@gmail.com>
2020-10-09 15:24:56 +0300

    Add Footer
    Basic Layout

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css"
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <img src="/img/github.svg" alt="github"/>
+            <a>Zergy</a>
+        </div>
+    )
+}
+
+export default Footer
