8e86cfbc03bfeba49c9beff3830944907ef6f3cc
Zergy <xxdarkbluedevilxx@gmail.com>
2020-10-14 15:43:24 +0300

    Added Context
    - Note context for the notes data
    - Local storage added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './App.css';
 import Home from "./components/Home";
-import Notes from "./Notes/Notes";
+import NewNote from "./NewNote/NewNote";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Notes from "./components/Notes";
 
+export const NoteContext = React.createContext({
+    'allNotes': [], addToNotesData: () => {}
+});
+
+const notesDataJson = localStorage.getItem('notesData');
+const allNotes = JSON.parse(notesDataJson) || [];
+
+const addToNotesData = (newNote) => {
+    allNotes.push(newNote);
+    const notesDataJson = JSON.stringify(allNotes);
+    localStorage.setItem('notesData', notesDataJson);
+};
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/newNote" component={Notes}/>
-            </Switch>
-        </Router>
+        <NoteContext.Provider value={{allNotes: allNotes, addToNotesData: addToNotesData}}>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/newNote" component={NewNote}/>
+                    <Route path="/allNotes" component={Notes}/>
+                </Switch>
+            </Router>
+        </NoteContext.Provider>
     );
 }
 
