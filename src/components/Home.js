3bb0068fca6942d38ae3b272fc486ff779aad113
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-02 10:26:03 +0200

    Rouging Changes
    Changed the routing from the home page to the other pages.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import "./Home.css"
 import Footer from "./Footer/Footer";
-
- import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Home() {
     return (
-            <div className={"home-container"}>
-                <div className="title-card">
-                    <h1 className="title">Welcome to Noted</h1>
-                    <Link to="/newNote"><button className="get-started-button">Get Started</button></Link>
-                </div>
-                <Footer/>
+        <div className={"home-container"}>
+            <div className="title-card">
+                <h1 className="title">Welcome to Noted</h1>
+                <Link to="/newNote">
+                    <button className="get-started-button">Get Started</button>
+                </Link>
             </div>
+            <Footer/>
+        </div>
     )
 }
 
