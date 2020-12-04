a358d7406888e0996318159aa3a799554a3e1af9
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-04 13:10:16 +0200

    Code Format

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 
 function Home() {
     return (
-        <div className={"home-container"}>
+        <div className="home-container">
             <div className="title-card">
                 <h1 className="title">Welcome to Noted</h1>
                 <Link to="/newNote">
