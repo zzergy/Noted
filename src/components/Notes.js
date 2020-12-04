5f61c0a8b3dc8f92fdcdea430c6eae0d5955958e
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-04 13:09:56 +0200

    All notes UI

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,38 @@
 import React, {useContext} from "react";
 import {NoteContext} from "../App";
+import "./Notes.css"
+import SideBar from "./SideBar/SideBar";
+import Footer from "./Footer/Footer";
 
 function Notes() {
     const {allNotes} = useContext(NoteContext);
 
     return (
-        <>
-            {/*Displays the saved notes*/}
-            {
-                allNotes.map(
-                    (item, index) => (
-                        <div key={index}>Title: {item.title} Note: {item.text}</div>
+        <div className="wrapper">
+            <div className="background"/>
+            <div className="sidebar-container">
+                <SideBar/>
+            </div>
+
+            <h1 className="all-notes-title">Your notes</h1>
+
+            <div className="all-notes">
+
+                {/*Displays the saved notes*/}
+                {
+                    allNotes.map(
+                        (item, index) => (
+                            <div key={index} className="note-item">
+                                Title: {item.title}
+                                <button className="note-item-view-button">View Note</button>
+                            </div>
+                        )
                     )
-                )
-            }
-        </>
+                }
+            </div>
+
+            <Footer/>
+        </div>
     );
 }
 
