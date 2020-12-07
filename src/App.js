df47183a94e58f0d3446aca938a8dab9f477bcd4
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-07 18:02:11 +0200

    Removed the previous context provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Home from "./components/Home";
 import NewNote from "./NewNote/NewNote";
@@ -7,46 +7,24 @@ import Notes from "./components/Notes";
 import About from "./components/AboutPage/About"
 import SideBar from "./components/SideBar/SideBar";
 import Footer from "./components/Footer/Footer";
+import NotesContextProvider from "./context/NotesContextProvider";
 
 
-//Create context with 2 values: allNotes that is an array and addToNotesData that is a function
-//This states what the context will want, you have to provide the values
-export const NoteContext = React.createContext({
-    'allNotes': [],
-    addToNotesData: () => {}
-});
-
-//Gets the current notesData from the local browser storage
-const notesDataJson = localStorage.getItem('notesData');
-
-//Parses the data to an object or an array
-const allNotes = JSON.parse(notesDataJson) || [];
-
-//Adding a new note to the local storage
-const addToNotesData = (newNote) => {
-    //Push the new note to the array allNotes
-    allNotes.push(newNote);
-
-    //Parse the array to json format
-    const notesDataJson = JSON.stringify(allNotes);
-
-    //Set the new data to the local storage
-    localStorage.setItem('notesData', notesDataJson);
-};
 
 function App() {
+
     return (
-        <NoteContext.Provider value={{allNotes: allNotes, addToNotesData: addToNotesData}}>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/newNote" component={NewNote}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/allNotes" component={Notes}/>
-                </Switch>
-            </Router>
-            <Footer/>
-        </NoteContext.Provider>
+            <NotesContextProvider>
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/newNote" component={NewNote}/>
+                        <Route path="/about" component={About}/>
+                        <Route path="/allNotes" component={Notes}/>
+                    </Switch>
+                </Router>
+                <Footer/>
+            </NotesContextProvider>
     );
 }
 
