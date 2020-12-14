c852d6bba6408265ebbb3fa41956958de12969c7
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-14 13:45:44 +0200

    Close button design

--- src/components/ViewNotePopUp/ViewNotePopUp.js
+++ src/components/ViewNotePopUp/ViewNotePopUp.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./ViewNotePopUp.css";
 import "../../CustomSliders.css"
+import * as GrIcons from "react-icons/gr"
 
 function ViewNotePopUp(props) {
 
     return (
         <div className={props.popUpActive ? "pop-up-container active" : "hidden"}>
-            <div className="exit-button">
-                <button onClick={props.clear}>X</button>
+            <div className="exit-button-container">
+                <button className="exit-button"><GrIcons.GrFormClose onClick={props.clear} size="25px"/></button>
             </div>
             <div className="note custom-slider">
                 <h3 style={{marginBottom: 25}}>{props.note.noteTitle}</h3>
