b0def86ac92891c339bb89adb62e444ffb446f89
Zergy <xxdarkbluedevilxx@gmail.com>
2020-12-21 15:31:02 +0200

    Updated About page

--- src/components/AboutPage/About.js
+++ src/components/AboutPage/About.js
@@ -6,8 +6,10 @@ function About () {
         <div className="main-container">
             <SideBar />
             <div className="about-container">
-                I have created a simple site for taking notes.
-                I've started it as a project so I can practise React, and to explore new technologies.
+                <p>
+                    I have created a simple site for taking notes.<br/>
+                    My goals with this project are to improve my coding skills using React.
+                </p>
             </div>
         </div>
     );
